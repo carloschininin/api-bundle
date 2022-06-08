4dc13ca164ff2f9e738b3d80b7515751da5375a2
Carlos Chininin <carloschininin@gmail.com>
2022-06-07 22:20:54 -0500

    patch tags controller api service

--- src/CarlosChinininApiBundle.php
+++ src/CarlosChinininApiBundle.php
@@ -9,24 +9,14 @@ declare(strict_types=1);
 
 namespace CarlosChininin\ApiBundle;
 
-use CarlosChininin\ApiBundle\DependencyInjection\CompilerPass\ControllersAsServices;
-use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-final class CarlosChinininApiBundle extends AbstractBundle
+class CarlosChinininApiBundle extends AbstractBundle
 {
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
         $container->import('../config/services.yaml');
-    }
-
-    public function build(ContainerBuilder $container)
-    {
-
-        $container->addCompilerPass(new ControllersAsServices(), PassConfig::TYPE_BEFORE_OPTIMIZATION, -1);
-
-        parent::build($container);
     }
 }
